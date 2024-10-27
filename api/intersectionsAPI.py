a1b57f89e591c43f6cf2d6b482bf7df5563006a5
Ethan Chester <43710677+ethan-chester@users.noreply.github.com>
2024-10-27 19:51:21 -0400

    Update image path

--- api/intersectionsAPI.py
+++ api/intersectionsAPI.py
@@ -48,40 +48,40 @@ def get_intersection():
                     "id": intersections[key]["id"],
                     "name": intersections[key]["name"],
                     "status": intersections[key]["status"],
-                    "timestamp": intersections[key]["timestamp"]
+                    "timestamp": intersections[key]["timestamp"],
+                    "image_url": intersections[key].get("imagepath", None)
                 }
             }
         geoJson["features"].append(new_feature)
-        geoJson["image_url"].append(intersections[key]["imagepath"])
         return jsonify(geoJson), 200
     except Exception as e:
         return jsonify({"error": f"An error occurred: {e}"}), 500
 
 
-@intersectionsAPI.route('/intersections/geo-json2', methods=['GET'])
-def get_intersection_image():
-    try:
-        # all_docs = intersections_ref.stream()
-        # intersections = {
-        # }
-        geoJson = {
-                    "type":"Feature",
-                    "geometry":{
-                        "type": "Polygon",
-                        "coordinates": [
-                            [
-                            [-100.0, 40.0],
-                            [-100.0, 40.0],
-                            [-100.0, 35.0],
-                            [-100.0, 35.0]
-                            ]
-                        ],
-                        "properties": {
-                            "name": "ELL1"
-                        }
-                    },
-                    "image_url": ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3c3JkyQWf2fFhd5qMPKw__Mm7FeAzXrxYmQ&s"]
-                }
+# @intersectionsAPI.route('/intersections/geo-json2', methods=['GET'])
+# def get_intersection_image():
+#     try:
+#         # all_docs = intersections_ref.stream()
+#         # intersections = {
+#         # }
+#         geoJson = {
+#                     "type":"Feature",
+#                     "geometry":{
+#                         "type": "Polygon",
+#                         "coordinates": [
+#                             [
+#                             [-100.0, 40.0],
+#                             [-100.0, 40.0],
+#                             [-100.0, 35.0],
+#                             [-100.0, 35.0]
+#                             ]
+#                         ],
+#                         "properties": {
+#                             "name": "ELL1"
+#                         }
+#                     },
+#                     "image_url": ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3c3JkyQWf2fFhd5qMPKw__Mm7FeAzXrxYmQ&s"]
+#                 }
 
         # for doc in all_docs:
         #     intersections[doc.id] = doc.to_dict()
@@ -105,7 +105,7 @@ def get_intersection_image():
         #     }
         # geoJson["features"].append(new_feature)
         # geoJson["image_url"].append(intersections[key]["imagepath"])
-        return jsonify(geoJson), 200
-    except Exception as e:
-        return jsonify({"error": f"An error occurred: {e}"}), 500
+    #     return jsonify(geoJson), 200
+    # except Exception as e:
+    #     return jsonify({"error": f"An error occurred: {e}"}), 500
     
